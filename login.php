dcc2f2e7e671f8eff7befe653b68bf4237b58f2c
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2023-03-13 19:46:58 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    
+        <title>registration form</title>
+        
+    </head>
+<body align="center">
+<form method="post" action="loginchk.php">
+<fieldset>
+<h1>Login page</h1>
+
+        <legend>USER LOGIN</legend>
+    <table border="0" align="center">
+        <tr>
+            <td>User: </td>
+            <td><input name="user" type="text"></td>
+        </tr>
+	
+        <tr>
+            <td>Password : </td>
+            <td><input name="password" type="password"></td>
+        </tr>
+<tr>
+            <td>First name : </td>
+            <td><input type="text" name="fname" ></td>
+        </tr>
+
+	<tr>
+            <td>last name : </td>
+            <td><input type="text" name="lname" ></td>
+        </tr>
+        <tr>
+	<td> </td>
+            <td><button type="submit">Login</button></td>
+        </tr>
+        
+    </table>
+</fieldset>
+</form>
+</body>
+</html>
